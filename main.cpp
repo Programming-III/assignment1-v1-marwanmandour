8f09484e810aef7c5da703dcbb621177feae90ec
marwanmandour <marwan.mandour@student.giu-uni.de>
2025-11-12 15:16:54 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,60 @@
 #include "Enclosure.h"
 #include "Visitor.h"
 #include <iostream>
+using namespace std;\
+//
+// Created by Lap on 11/12/2025.
+//
+
+#include "animal.h"
+#include <iostream>
 using namespace std;
+
+public static void display() {
+     cout << name <<"name " << endl;
+     cout <<age  <<"age" << endl;
+     cout <<name  <<"hunger status " << endl;
+};
+public static bool isHungry() {
+     isHungry =false ;
+}
+public static void feed() {
+     if (animal==isHungry) {
+          return true;
+     else
+
+
+          return false;
+
+     };
+//
+// Created by Lap on 11/12/2025.
+//
+
+#define UNTITLED_ENCLOSURE_H
+#include "Enclosure.h"
+using namespace std
+public static void addAnimal(Animal*a) {
+    if (Animal ==full) {
+        return zoo is full;
+        else
+        {
+push.Animal(Animal)
+
+        }
+        public static void displayanimals() {
+            cout<<Animal<<endl;
+        }
+
+    }
+}
+
+
+
+
+
+
+
 
 
 // ============== MAIN FUNCTION ==============
