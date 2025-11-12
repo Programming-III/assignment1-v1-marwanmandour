7e88ae754c42c4aff59ea4bf17b3e53cb3046bbc
marwanmandour <marwan.mandour@student.giu-uni.de>
2025-11-12 15:14:56 +0200

    Update Animal.h

--- Animal.h
+++ Animal.h
@@ -1,11 +1,23 @@
-#ifndef ANIMAL_H
-#define ANIMAL_H
+//
+// Created by Lap on 11/12/2025.
+//
 
-#include <string>
-using namespace std;
+#ifndef UNTITLED_ANIMAL_H
+#define UNTITLED_ANIMAL_H
+#include <iostream>
+using namespace std
 
-//define animal class here 
+
+class animal {
+private:
+    string name ;
+    int age ;
+    bool isHungry;
+public:
+    void display();
+    void feed();
+    void isHungry();
+};
 
 
-
-#endif
+#endif //UNTITLED_ANIMAL_H
