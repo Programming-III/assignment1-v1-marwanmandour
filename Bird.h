b0fb0fbf2d4bcf6b6c3cc036a1df7fd9c234feca
marwanmandour <marwan.mandour@student.giu-uni.de>
2025-11-12 15:26:03 +0200

    Update Bird.h

--- Bird.h
+++ Bird.h
@@ -1,14 +1,14 @@
 #ifndef BIRD_H
 #define BIRD_H
-
 #include "Animal.h"
 
-//define class bird here 
-
-
-
-
-
-
-
+class Bird : public Animal {
+private:
+    float wingSpan;
+public:
+    Bird();
+    Bird(string n, int a, bool h, float w);
+    ~Bird();
+    void display() const override;
+};
 #endif
