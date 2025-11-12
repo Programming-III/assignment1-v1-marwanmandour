5a2e39c2c1314b45f348067da8d06745c710cbfb
marwanmandour <marwan.mandour@student.giu-uni.de>
2025-11-12 15:25:48 +0200

    Update Mammal.h

--- Mammal.h
+++ Mammal.h
@@ -1,13 +1,14 @@
 #ifndef MAMMAL_H
 #define MAMMAL_H
-
 #include "Animal.h"
 
-//define mammal class here
-
-
-
-
-
-
+class Mammal : public Animal {
+private:
+    string furColor;
+public:
+    Mammal();
+    Mammal(string n, int a, bool h, string f);
+    ~Mammal();
+    void display() const override;
+};
 #endif
